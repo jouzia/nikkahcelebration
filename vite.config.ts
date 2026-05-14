e2e2fbd3d9d707e8babba202fbee5da9ff65f20a
Jouzia <jouziaafreenh@gmail.com>
2026-05-14 22:16:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // UNCOMMENTED AND SET TO YOUR REPO NAME
-  base: '/nikkahcelebration/', 
-  
   plugins: [react(), tsconfigPaths()],
   build: {
-    outDir: "dist",
+    outDir: "dist", // This MUST match the Vercel setting
   },
 });
